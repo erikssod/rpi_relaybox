ce34e04a9efa96bfb766c35af98d7f30fdac05e5
Daniel Eriksson <daniel.eriksson@synchrotron.org.au>
2019-12-30 13:52:10 +1100

    more boilerplate

--- solenoids/app.py
+++ solenoids/app.py
@@ -1,4 +1,28 @@
 #!/usr/bin/env python
 
-import pyYAML
+import yaml, sys, logbook
+
+class Actuate:
+    def __init__(self):
+        logbook.StreamHandler(sys.stdout).push_application()
+        self.log = logbook.Logger(self.__class__.__name__)
+        logbook.set_datetime_format("local")
+        self.log.info('Logbook started')
+
+        try:
+            with open('config.yaml', 'r') as stream:
+                self.cfg = yaml.load(stream, Loader=yaml.FullLoader)
+        except FileNotFoundError:
+            self.log.critical('Config file not found')
+            sys.exit()
+
+        self.log.level = eval('logbook.'+self.cfg['debug']['loglevel'])
+
+    def check(self):
+        self.log.debug(self.cfg)
+
+if __name__ == '__main__':
+    a = Actuate()
+    a.check()
+
 
