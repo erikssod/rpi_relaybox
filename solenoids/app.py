6bd5439a7b1b91c04da2c33d1733307e480a3a4d
Daniel Eriksson <daniel.eriksson@synchrotron.org.au>
2019-12-30 22:36:10 +1100

    Testing LED method

--- solenoids/app.py
+++ solenoids/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import yaml, sys, logbook
+import yaml, sys, logbook, gpiozero
 
 class Actuate:
     def __init__(self):
@@ -18,8 +18,15 @@ class Actuate:
 
         self.log.level = eval('logbook.'+self.cfg['debug']['loglevel'])
 
+        self.setup()
+
     def check(self):
         self.log.debug(self.cfg)
+
+    def setup(self):
+        self.pair1 = gpiozero.LED(self.cfg['relay_ctl_pin']['beds1n2'])
+        self.pair2 = gpiozero.LED(self.cfg['relay_ctl_pin']['beds3n4'])
+        self.spikes = gpiozero.LED(self.cfg['relay_ctl_pin']['spikes'])
 
 if __name__ == '__main__':
     a = Actuate()
