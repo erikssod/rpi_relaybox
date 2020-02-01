5aa29ca7ed0390f737b830b3194c5684c80436d1
erikssod <ericsson.daniel@gmail.com>
2020-02-01 23:44:05 +1100

    added pint units

--- pressure_monitor/app.py
+++ pressure_monitor/app.py
@@ -3,6 +3,7 @@
 import time, board, busio, csv
 import adafruit_ads1x15.ads1115 as ADS
 from adafruit_ads1x15.analog_in import AnalogIn
+import pint
 
 # Create the I2C bus
 i2c = busio.I2C(board.SCL, board.SDA)
@@ -13,18 +14,37 @@ ads = ADS.ADS1115(i2c)
 # Create single-ended input on channel 0
 chan0 = AnalogIn(ads, ADS.P0)
 
-fieldnames = ['timestamp','val']
+fieldnames = ['timestamp','val', 'voltage', 'pressure']
+
+ureg = pint.UnitRegistry()
 
 with open('data.csv', 'w') as csv_file:
     csv_writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
     csv_writer.writeheader()
+
+def to_V(val):
+    volts = val * ureg.volt
+    return volts.to_compact()
+
+def to_Pa(V):
+    offset = 0.5
+    factor = 1.6/4.5 * 10**6
+    magnitude = (V - offset) * factor
+    pressure = magnitude * ureg.pascal
+    return pressure.to_compact()
 
 while True:
     with open('data.csv','a') as csv_file:
         csv_writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
 
         payload = {'timestamp':time.ctime(),
-                   'val':chan0.value}
+                   'val':chan0.value,
+                   'voltage':to_V(chan0.voltage),
+                   'pressure':to_Pa(chan0.voltage),
+                   }
 
         csv_writer.writerow(payload)
+        print(payload)
         time.sleep(1)
+
+
